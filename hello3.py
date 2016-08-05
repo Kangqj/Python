589bca13b517ea76c4336980b4d3b01180731f4b
Kangqijun <qijun.kang@coverme.ws>
2016-08-05 17:15:44 +0800

    xxx

--- hello3.py
+++ hello3.py
@@ -120,6 +120,80 @@ print('AAA.name', AAA.name)
 del a.name
 print('a.name3', a.name)
 print('AAA.name', AAA.name)
+
+#面向对象高级编程
+#试用__slots__
+class Student(object):
+	__slots__=('name', 'age',)
+s = Student()
+s.name = 'aaa'
+s.age = 23
+
+class GraduateStudnet(object):
+	pass
+g = GraduateStudnet()
+g.score = 999
+
+#试用@property
+class Student(object):
+	@property
+	def score(self):
+		return self._score
+	
+	@score.setter
+	def score(self, value):
+		if not isinstance(value, int):
+			raise ValueError('score must be an interger!')
+		if value < 0 or value > 100:
+			raise ValueError('score must between 0 ~ 100')
+		self._score = value
+	@property
+	def birth(self):
+		return self._birth
+	
+	@birth.setter
+	def birth(self, value):
+		self._birth = value
+
+	@property
+	def age(self):
+		return 2015 - self._birth
+
+
+s = Student();
+s.score = 60
+
+
+class Screen(object):
+
+	@property
+	def width(self):
+		return self._width
+	@width.setter
+	def width(self, value):
+		self._width = value;
+
+	@property
+	def height(self):
+		return self._height
+	@height.setter
+	def height(self, value):
+		self._height = value;
+
+	@property
+	def resolution(self):
+		return self._width * self._height
+
+s = Screen()
+s.width = 1024
+s.height = 768
+print(s.resolution)
+
+#多重继承
+
+
+		
+
 		
 
 
