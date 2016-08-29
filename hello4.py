a20ba653b17d0e065e4d1a02e225f8ea8b2b931e
Kangqijun <qijun.kang@coverme.ws>
2016-08-29 18:03:01 +0800

    a

--- hello4.py
+++ hello4.py
@@ -6,3 +6,28 @@
 
 
 #IO编程
+
+#读取文本文件，且是utf-8编码格式的文本文件
+try:
+	f = open('/Users/kangqijun/Python/test1.txt', 'r')
+	print('---\n' + f.read())
+finally:
+	if f:
+		f.close()
+
+with open('/Users/kangqijun/Python/test1.txt', 'r') as f:
+	print('---\n' + f.read())
+
+f = open('/Users/kangqijun/Python/test1.txt', 'r')
+for line in f.readlines():
+	print('---\n' + line.strip())# 把末尾的'\n'删掉
+f.close()
+
+f = open('/Users/kangqijun/Python/test1.txt', 'r')
+# print('1---\n' + f.readline(8))
+print('2---\n' + f.read(6))
+
+#读取二进制文件：图片，视频，等,使用‘rb’
+# f = open('/Users/kangqijun/Python/test.png', 'rb')
+# print(f.read())# 十六进制表示的字节
+
