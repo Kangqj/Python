2ddf655d96b9b6e2ca363d433ba62502c55d66ec
Kangqijun <qijun.kang@coverme.ws>
2016-09-05 17:59:23 +0800

    aaa

--- hello4.py
+++ hello4.py
@@ -27,7 +27,23 @@ f = open('/Users/kangqijun/Python/test1.txt', 'r')
 # print('1---\n' + f.readline(8))
 print('2---\n' + f.read(6))
 
+f = open('/Users/kangqijun/Python/test1.txt', 'r', encoding='utf-8', errors='ignore')
+print(f.read())
+
+f = open('/Users/kangqijun/Python/test1.txt', 'r', encoding='gbk', errors='ignore')
+print(f.read())
+
 #读取二进制文件：图片，视频，等,使用‘rb’
 # f = open('/Users/kangqijun/Python/test.png', 'rb')
 # print(f.read())# 十六进制表示的字节
+
+#写文件
+
+with open('/Users/kangqijun/Python/test1.txt', 'w') as f:
+	f.write('hello, world')
+
+with open('/Users/kangqijun/Python/test1.txt', 'r') as f:
+	print(f.read())
+
+
 
