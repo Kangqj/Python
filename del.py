fb5eb926d39c4c1267678a7325720021e0de9d6d
Kangqijun <qijun.kang@coverme.ws>
2016-09-07 16:30:34 +0800

    modify

--- del.py
+++ del.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#两个.py文件相互调用
 import os
+from del2 import *
 
 # 删除文件： 
 # os.remove()
@@ -11,5 +13,6 @@ import os
 # 递归删除空目录： 
 # os.removedirs()
 
-os.listdir('/Users/kangqijun')
+print('---------')
+filterDir()
 
--- del2.py
+++ del2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #http://blog.csdn.net/cnmilan/article/details/17968391
-#删除目标路径下的第一个文件(夹)
+#删除目标路径下的某一文件(夹)
 
 import os
 import shutil
@@ -11,17 +11,25 @@ delDir = "/Users/kangqijun/aaa"
 def removeFile(path):
 	if os.path.isfile(path):
 		os.remove(path)
-		print("file:" + path + " was removed!")
+		print("delete file:" + path + " was removed!")
 	elif os.path.isdir(path):
 		shutil.rmtree(path, True)
-		print("directory: " + path +" was removed!")
+		print("delete directory: " + path +" was removed!")
 
-delList = []
-delList = os.listdir(delDir)
+def filterDir():
+	delList = []
+	delList = os.listdir(delDir)
+	if len(delList) > 0:
+		for name in delList:
+			if '2016' in name:
+				removeFile(os.path.join(delDir, name))
+				break
+	else:
+		print("nothing to remove!")
 
-if len(delList) > 0:
-	filePath = os.path.join(delDir, delList[0])
-	print('name:' + os.path.basename(filePath))
-	removeFile(filePath)
-else:
-	print("nothing to remove!")
+# if len(delList) > 0:
+# 	print(delList)
+# 	filePath = os.path.join(delDir, delList[0])
+# 	removeFile(filePath)
+# else:
+# 	print("less than 20!")
